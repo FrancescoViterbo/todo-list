17809b466dc9689a1e715c7fa72e7542f18a8352
FrancescoViterbo <franc.viterbo@gmail.com>
2020-10-16 23:40:35 +0200

    Added localStorage saving and simple login system

--- script/script.js
+++ script/script.js
@@ -5,14 +5,59 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const alerts = document.getElementById('alerts');
 const filterOption = document.querySelector(".filter-todo");
+const logoutBtn = document.getElementById("logout");
+const loginBtn = document.getElementById("login");
 
 // Event Listeners
 
+document.addEventListener('DOMContentLoaded', getName);
+loginBtn.addEventListener('click', saveName);
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener('click', filterTodo)
+filterOption.addEventListener('click', filterTodo);
+logoutBtn.addEventListener('click', logout);
 
 // Functions
+
+function saveName(){
+    if (localStorage.getItem("username") === null){
+        var name = prompt("Please enter your name", "Your Name Here");  
+        logoutBtn.style.display = "none";
+        if (name != null) {
+            document.getElementById("title").innerHTML = name + "'s Todo List";
+            saveLocalName(name);
+            logoutBtn.style.display = "inline-block";
+        } else {
+            document.getElementById("title").innerHTML = "Anonymous Todo List";
+            loginBtn.style.display = "inline-block";
+        }
+        loginBtn.style.display = "none";
+    } else {
+        name = JSON.parse(localStorage.getItem("username")); 
+        document.getElementById("title").innerHTML = name + "'s Todo List";
+        loginBtn.style.display = "none";
+        logoutBtn.style.display = "inline-block";
+    }
+}
+
+function getName(){
+    if (localStorage.getItem("username") !== null){
+        logoutBtn.style.display = "inline-block";
+        name = JSON.parse(localStorage.getItem("username")); 
+        document.getElementById("title").innerHTML = name + "'s Todo List";
+        loginBtn.style.display = "none";
+    } else {
+        logoutBtn.style.display = "none";
+        loginBtn.style.display = "inline-block";
+    }
+}
+
+function logout(){
+    localStorage.removeItem('username');
+    document.getElementById("title").innerHTML = "Anonymous Todo List.";
+    logoutBtn.style.display = "none";
+}
 
 function addTodo(event) {
     event.preventDefault();
@@ -20,26 +65,39 @@ function addTodo(event) {
         alerts.innerHTML = "You must write something before creating a new task";
     } else {
         alerts.innerHTML = "";
-        const todoDiv = document.createElement('div');
-        todoDiv.classList.add("todo");
-        // todoDiv.classList.add("uncompleted");
-        const newTodo = document.createElement('li');
-        newTodo.innerHTML = todoInput.value;
-        newTodo.classList.add("todo-item");
-        todoDiv.appendChild(newTodo);
-        // Complete button
-        const completedButton = document.createElement('button');
-        completedButton.innerHTML = '<img src="img/icons8-check-file-64.png" alt="completed">';
-        completedButton.classList.add("complete-btn");
-        todoDiv.appendChild(completedButton);
-        // Delete button
-        const deleteButton = document.createElement('button');
-        deleteButton.innerHTML = '<img src="img/icons8-empty-trash-64.png" alt="completed">'
-        deleteButton.classList.add("delete-btn");
-        todoDiv.appendChild(deleteButton);
-        // Append to list
-        todoList.appendChild(todoDiv);
-        todoInput.value = "";
+        let todos;
+        if (localStorage.getItem('todos') === null){
+            console.log("item created");
+        } else {
+            todos = JSON.parse(localStorage.getItem('todos'));
+            if (todos.includes(todoInput.value)){
+                alerts.innerHTML = "You already created a task with this name";
+            } else {
+                // Add todo to local storage
+                saveLocalTodos(todoInput.value);
+                const todoDiv = document.createElement('div');
+                todoDiv.classList.add("todo");
+                // todoDiv.classList.add("uncompleted");
+                const newTodo = document.createElement('li');
+                newTodo.innerHTML = todoInput.value;
+                newTodo.classList.add("todo-item");
+                todoDiv.appendChild(newTodo);
+                // Complete button
+                const completedButton = document.createElement('button');
+                completedButton.innerHTML = '<img src="img/icons8-check-file-64.png" alt="completed">';
+                completedButton.classList.add("complete-btn");
+                todoDiv.appendChild(completedButton);
+                // Delete button
+                const deleteButton = document.createElement('button');
+                deleteButton.innerHTML = '<img src="img/icons8-empty-trash-64.png" alt="completed">'
+                deleteButton.classList.add("delete-btn");
+                todoDiv.appendChild(deleteButton);
+                // Append to list
+                todoList.appendChild(todoDiv);
+                todoInput.value = "";
+
+            }
+        }
     }
 }
 
@@ -57,6 +115,7 @@ function deleteCheck(e) {
             item.classList.add("align-right");
             check.remove();
             text.classList.add("fall");
+            removeLocalTodos(todo);
             text.addEventListener('animationend', function(){
                 todo.remove();
             })
@@ -64,7 +123,6 @@ function deleteCheck(e) {
     } else if (item.classList[0] === "complete-btn") {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
-        // todo.classList.remove("uncompleted");
     }
 }
 
@@ -92,4 +150,64 @@ function filterTodo(e) {
                 break;
         }
     });
+}
+
+function saveLocalName(name){
+    localStorage.setItem('username', JSON.stringify(name));
+}
+
+function saveLocalTodos(todo){
+    // Check if the storage is already populated
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos(){
+    let todos;
+    // Check if the storage is already populated
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo){
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add("todo");
+        // todoDiv.classList.add("uncompleted");
+        const newTodo = document.createElement('li');
+        newTodo.innerHTML = todo;
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+        // Complete button
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<img src="img/icons8-check-file-64.png" alt="completed">';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+        // Delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<img src="img/icons8-empty-trash-64.png" alt="completed">'
+        deleteButton.classList.add("delete-btn");
+        todoDiv.appendChild(deleteButton);
+        // Append to list
+        todoList.appendChild(todoDiv);
+    });
+}
+
+function removeLocalTodos(todo){
+    // Check if the storage is already populated
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    const todoName = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoName), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
